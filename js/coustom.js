18774463609d3c23fe98515c757e6d61648b85b5
webdevbiswaajit <biswajitsikdar51@gmail.com>
2023-02-10 15:17:43 +0600

    Map amd Subscribe area design

--- js/coustom.js
+++ js/coustom.js
@@ -1,4 +1,4 @@
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".sliderSwiper", {
   loop: true,
   autoplay: {
     delay: 3500,
@@ -13,7 +13,51 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+// =====================Test-swiper==============
+var swiper = new Swiper(".testSwiper", {
+  loop: true,
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+  slidesPerView: 2,
+  spaceBetween: 60,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
 
+// ====================management===============
+var swiper = new Swiper(".mymanagement", {
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+// ========================Consultant================
+
+var swiper = new Swiper(".consultantSwiper", {
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  slidesPerView: 4,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
 // =================Swiper===============
 $(".count").each(function () {
   $(this)
